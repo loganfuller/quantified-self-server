52b072d29846403c7c2eba716ebd234ce2889c8b
Logan Fuller <fuller.logan@gmail.com>
2014-08-07 22:10:39 -0400

    basic hello world server

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var restify = require("restify");
 
 var server = restify.createServer();
 
-server.put("/", function(req, res, next) {
+server.get("/", function(req, res, next) {
     res.send("Hello, world!");
     return next();
+});
+
+server.listen(process.env.HTTP_PORT || 8080, function() {
+    console.log('%s listening at %s', server.name, server.url);
 });
